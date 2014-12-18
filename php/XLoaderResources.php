fc6edaff775475101ffe4bc441b5113bd41da7e4
Cosmo <daixianceng@gmail.com>
2014-12-18 14:08:13 +0800

    修复XLoaderResources::setUrl 的返回值问题

--- php/XLoaderResources.php
+++ php/XLoaderResources.php
@@ -3,20 +3,48 @@ require_once 'XLoader.php';
 
 class XLoaderResources
 {
+	/**
+	 * 图片路径
+	 * 
+	 * @var string
+	 */
 	protected $_path;
 	
+	/**
+	 * 图片路径URL
+	 * 
+	 * @var string
+	 */
 	protected $_url;
 	
+	/**
+	 * 将要发送给客户端的图片数据
+	 * 
+	 * @var array
+	 */
 	protected $_images = array();
 	
 	public function __construct()
 	{}
 	
+	/**
+	 * 获取图片路径
+	 * 
+	 * @return string
+	 */
 	public function getPath()
 	{
 		return $this->_path;
 	}
 	
+	/**
+	 * 设置图片路径
+	 * 
+	 * 请不要在路径末尾加上路径分隔符"/"或"\"
+	 * 
+	 * @param string $path
+	 * @return XLoaderResources
+	 */
 	public function setPath($path)
 	{
 		$this->_path = $path;
@@ -24,18 +52,42 @@ class XLoaderResources
 		return $this;
 	}
 	
+	/**
+	 * 获取图片路径URL
+	 * 
+	 * @return string
+	 */
 	public function getUrl()
 	{
 		return $this->_url;
 	}
 	
+	/**
+	 * 设置图片路径URL
+	 * 
+	 * 请不要在路径末尾加上路径分隔符"/"
+	 * 
+	 * @param string $url
+	 * @return XLoaderResources
+	 */
 	public function setUrl($url)
 	{
 		$this->_url = $url;
 		
-		return $this->_url;
+		return $this;
 	}
 	
+	/**
+	 * 增加图片数据
+	 * 
+	 * 参数格式：array(
+	 *           'The image file name' => 'The image description',
+	 *           ...
+	 *       )
+	 * 
+	 * @param array $images
+	 * @return XLoaderResources
+	 */
 	public function addImages($images)
 	{
 		static $imageNames = array();
@@ -62,6 +114,9 @@ class XLoaderResources
 		return $this;
 	}
 	
+	/**
+	 * 发送图片数据到客户端
+	 */
 	public function send()
 	{
 		echo json_encode($this->_images);
